d467924c3391704c6d790fca4363e6533abeaa33
Jose Martinez <jmarti1997.jm@gmail.com>
2023-03-04 15:43:24 -0600

    addFieldRequest

--- server/src/app.py
+++ server/src/app.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
+from google.cloud.firestore import GeoPoint
 import placement
 
 class NumpyEncoder(json.JSONEncoder):
@@ -20,16 +21,20 @@ cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 todo_ref = db.collection('todos')
+fields = db.collection('fields_test')
 usersCollection = db.collection('users')
+
+
 
 @app.route("/",methods = ['GET'])
 def start():
-    return "server start"
+     return jsonify({"success": True}), 200
+
 
 @app.route("/signup", methods =['GET','POST'])
 def signUp():
     try:
-        newUserId = request.args.get('userID')
+        newUserId = request.get_json()['userID']
         jsonEntry = {
             'first_name': 'Jose',
             'last_name':'Martinez'
@@ -39,7 +44,51 @@ def signUp():
     except Exception as e:
         return f"An Error Occurred: {e}"
 
+@app.route("/signin", methods =['GET','POST'])
+def signIn():
+    global currentUserReference
+    currentUser = request.get_json()['userID']
+    currentUserReference = f"users/'{currentUser}'"
+    print("currentUser",currentUserReference)
+    return jsonify({"success": True}), 200
 
+#sample request_body
+# {
+#     "geopoint_1": "30|50",
+#     "geopoint_2": "30|60",
+#     "geopoint_3": "30|70",
+#     "geopoint_4": "30|80"
+# }
+@app.route("/addField", methods =['GET','POST'])
+def addField():
+    try:
+        lat,lng = (tuple([float(x) for x in request.get_json()['geopoint_1'].split("|")]))
+        firstGeopoint = GeoPoint(lat,lng)
+
+        lat,lng = (tuple([float(x) for x in request.get_json()['geopoint_2'].split("|")]))
+        secondGeopoint = GeoPoint(lat,lng)
+        
+        lat,lng = (tuple([float(x) for x in request.get_json()['geopoint_3'].split("|")]))
+        thirdGeopoint = GeoPoint(lat,lng)
+
+        lat,lng = (tuple([float(x) for x in request.get_json()['geopoint_4'].split("|")]))
+        fourthGeopoint = GeoPoint(lat,lng)
+
+        docJsonEntry = {
+            "user_id": db.document(currentUserReference).id,
+            "field_name":'test_field',
+            "first_point" : firstGeopoint,
+            "second_point": secondGeopoint,
+            "third_point": thirdGeopoint,
+            "fourth_point": fourthGeopoint
+        }
+        fields.document().set(docJsonEntry)
+        return jsonify({"success": True})
+    except Exception as e:
+        return f"An Error Occurred: {e}"
+
+ 
+    
 @app.route('/add', methods=['POST'])
 def create():
     """
